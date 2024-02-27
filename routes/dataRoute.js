334e56b396f271b99036bf4b686c77a090b2acbf
arbaap <arbaadhipamungkas@gmail.com>
2024-02-27 17:26:34 +0700

    asd

--- routes/dataRoute.js
+++ routes/dataRoute.js
@@ -25,4 +25,17 @@ router.post("/kirimData", async (req, res) => {
     res.status(500).json({ message: "Gagal menyimpan data semua" });
   }
 });
+
+router.get("/getDataAll", async (req, res) => {
+  try {
+    const data = await Data.find({});
+    console.log("Berhasil mengambil data all");
+    res.status(200).json(data);
+  } catch (err) {
+    console.log("Gagal mengambil data all:", err);
+    res.status(500).json({ message: "Gagal mengambil data all" });
+  }
+});
+
+
 module.exports = router;
