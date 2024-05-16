506932759e010a2152b333dabe8fef40864fc985
akito <anandaalieff@gmail.com>
2024-05-17 05:12:01 +0700

    biar keliatan diweb

--- mongoDB.js
+++ mongoDB.js
@@ -2,9 +2,6 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-// var mongoURL =
-//   "mongodb+srv://arduino:arduino@cluster0.93sie2d.mongodb.net/lora";
-
 var mongoURL =
   "mongodb+srv://pettracker:gtyu12345@pettracker.p2wjz7i.mongodb.net/lora";
 
