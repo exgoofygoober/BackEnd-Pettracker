4545e825b89007fe48a9a744f21f174722dfce6b
akito <anandaalieff@gmail.com>
2024-05-28 17:16:10 +0700

    balik lagi sebelum diganti

--- routes/loraRoute.js
+++ routes/loraRoute.js
@@ -56,26 +56,4 @@ router.get("/limitLora", async (req, res) => {
       .json({ message: "Terjadi kesalahan saat mengambil data LoRa" });
   }
 });
-
-
-router.get("/pageLora", async (req, res) => {
-  try {
-    let limit = req.query.limit ? parseInt(req.query.limit) : 1;
-    let page = req.query.page ? parseInt(req.query.page) : 0;
-
-    const offset = page * limit;
-
-    const data = await Lora.find({})
-      .sort({ createdAt: -1 })
-      .skip(offset)
-      .limit(limit);
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ message: "Terjadi kesalahan saat mengambil data LoRa" });
-  }
-});
-
 module.exports = router;
